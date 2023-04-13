ec9685d8225471e3b8376039d468eaabb8bdbc38
Foxiel <105858467+Foxiel@users.noreply.github.com>
2023-04-13 22:28:36 +0200

    Initial commit

---
+++ Day2/Day2/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] lines = File.ReadAllLines("./day2.txt");
+
+int valid = 0;
+
+foreach(string line in lines){
+    
+}
+
+
+
+// TODO:
+// Split array item into 3 parts 1 being the numbers, 1 being the letter it is supposed to find and 1 being the row of letters
+
+// 1. Put the amount of times that the letter appears in the last string to a variable
+// 2. Split the first array into 2 seperate variables 
+// 3. Compare the 2 separate variables with the amount of times that it is valid
+// 4. If it is valid, do valid++
+// 5. write valid to the console
+
+// TIP: use this website https://www.c-sharpcorner.com/UploadFile/mahesh/split-string-in-C-Sharp/
