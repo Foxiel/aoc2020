56c78ec4b2b4511717b7c7c2494185297b2a998c
Fabian <fabianverhagen2011@gmail.com>
2023-04-17 14:13:03 +0200

    Day 2 part 2

--- Day2/Day2/Program.cs
+++ Day2/Day2/Program.cs
@@ -1,22 +1,64 @@
 ﻿string[] lines = File.ReadAllLines("./day2.txt");
 
 int valid = 0;
+string[] separator = { " ", "-", ":" };
 
+// PART 1:
+// foreach(string line in lines)
+// {
+//     string[] strings = line.Split(separator, 4, StringSplitOptions.None);
+//     foreach (string str in strings)
+//     {
+//         int min = int.Parse(strings[0]);
+//         int max = int.Parse(strings[1]);
+//         char letter = char.Parse(strings[2]);
+//         string password = strings[3];
+//
+//         int count = password.Count(x => x == letter);
+//         if (count >= min && count <= max)
+//         {
+//             valid++;
+//             break;
+//         }
+//     }
+// }
+
+// PART 2:
 foreach(string line in lines)
 {
-    line.Split();
+    string[] strings = line.Split(separator, 4, StringSplitOptions.None);
+    foreach (string str in strings)
+    {
+        int min = int.Parse(strings[0]);
+        int max = int.Parse(strings[1]);
+        char character = char.Parse(strings[2]);
+        string password = strings[3].Trim();
+        bool first = false;
+        bool second = false;
+
+        for (int i = 0; i < password.Length; i++)
+        {
+            char letter = password[i];
+            if (letter == character && i == min - 1)
+            {
+                first = true;
+            }
+            else if (letter == character && i == max - 1)
+            {
+                second = true;
+            }
+        }
+        if (first && !second)
+        {
+            valid++;
+            break;
+        }
+        if (second && !first)
+        {
+            valid++;
+        }
+        break;
+    }
+    
 }
-Console.WriteLine("Done");
-
-
-
-// TODO:
-// Split array item into 3 parts 1 being the numbers, 1 being the letter it is supposed to find and 1 being the row of letters
-
-// 1. Put the amount of times that the letter appears in the last string to a variable
-// 2. Split the first array into 2 seperate variables 
-// 3. Compare the 2 separate variables with the amount of times that it is valid
-// 4. If it is valid, do valid++
-// 5. write valid to the console
-
-// TIP: use this website https://www.c-sharpcorner.com/UploadFile/mahesh/split-string-in-C-Sharp/
+Console.WriteLine("Valid = " + valid);
